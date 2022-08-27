c28a9eb5549b957b0978149fcb1191a9fdb95c17
zoppatorsk <49608884+zoppatorsk@users.noreply.github.com>
2022-08-27 14:46:26 +0200

    added dotenv n stuff

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -28,6 +28,7 @@
 	});
 
 	const socket = io('http://localhost:3000');
+	//const socket = io();
 
 	socket.on('connect', () => {
 		$activeComponent = 'Start';
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -5,18 +6,24 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const path = require('path');
 
-//dev shite with cors support.. remember to change this later
-const io = new Server(server, {
-	cors: {
-		origin: ['http://localhost:5173', 'http://localhost:5174'],
-		credentials: true,
-	},
-});
+//if run in dev put in the cors junk
+const ioConfig =
+	process.env.NODE_ENV == 'dev'
+		? {
+				cors: {
+					origin: ['http://localhost:5173', 'http://localhost:5174'],
+					credentials: true,
+				},
+		  }
+		: {};
+
+const io = new Server(server, ioConfig);
 require('./eventHandler.js')(io);
 
 app.use(express.static(path.join(__dirname, 'public'))); //serve static files from the public folder so can run frontend on the same server
 
-server.listen(3000, () => {
-	console.log('listening on *:3000');
+const port = process.env.PORT;
+server.listen(port, () => {
+	console.log(`listening on *:${port}`);
 });
 module.exports = io;
